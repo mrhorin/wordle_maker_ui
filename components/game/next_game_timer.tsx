6226eb804092e05aa802eef1997a983f70e334ee
mrhorin <u0813mb@gmail.com>
2022-06-10 02:11:11 +0900

    Prevent 'Text content did not match' error

--- components/game/next_game_timer.tsx
+++ components/game/next_game_timer.tsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react'
 
 const NextGameTimer = () => {
-  const [now, setNow] = useState<Date>(new Date())
-  const [expiry] = useState<Date>(() => {
-    return new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1)
-  })
+  const [now, setNow] = useState<Date>()
+  const [expiry, setExpiry] = useState<Date>()
+
+  // Prevent 'Text content did not match' error
+  useEffect(() => {
+    const now: Date = new Date()
+    setNow(now)
+    setExpiry(new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1))
+  }, [])
 
   useEffect(() => {
-    if (expiry > now) setTimeout(() => { setNow(new Date()) }, 1000)
+    if (now && expiry && expiry > now) setTimeout(() => { setNow(new Date()) }, 1000)
   }, [now])
 
-  const hours = ('00' + (23 - now.getHours())).slice(-2)
-  const mins = ('00' + (59 - now.getMinutes())).slice(-2)
-  const secs = ('00' + (59 - now.getSeconds())).slice(-2)
+  const hours = now ? ('00' + (23 - now.getHours())).slice(-2) : '00'
+  const mins = now ? ('00' + (59 - now.getMinutes())).slice(-2) : '00'
+  const secs = now ? ('00' + (59 - now.getSeconds())).slice(-2) : '00'
 
   return (
     <div className='timer'>
