0ea7200193327e75e70d3c51836ffe659965008b
mrhorin <u0813mb@gmail.com>
2022-10-19 11:07:55 +0900

    Fix key color priority on Keyboard

--- hooks/useGetKeyStatus.ts
+++ hooks/useGetKeyStatus.ts
@@ -2,12 +2,30 @@ import type { Tile, TileStatus } from 'types/global'
 
 // useGetKeyStatus
 export default () => {
+  const PRIORITY = {
+    'EMPTY': 0,
+    'PRESENT': 1,
+    'ABSENT': 2,
+    'CORRECT': 2,
+  }
+
   const getKeyStatus = (letter: string, tilesTable: Tile[][]): TileStatus => {
     let status: TileStatus = 'EMPTY'
     for (let i = 0; i < tilesTable.length; i++){
+      // Columns
       for (let j = 0; j < tilesTable[i].length; j++) {
-        if (tilesTable[i][j].letter == letter) status = tilesTable[i][j].status
+        // Rows
+        const currentTile: Tile = tilesTable[i][j]
+        if (currentTile.letter == letter) {
+          if (currentTile.status == 'CORRECT' || currentTile.status == 'ABSENT') {
+            status = currentTile.status
+            break
+          } else if (currentTile.status == 'PRESENT' && status == 'EMPTY') {
+            status = currentTile.status
+          }
+        }
       }
+      if (status == 'CORRECT' || status == 'ABSENT') break
     }
     return status
   }
