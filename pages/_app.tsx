70a7541c3fff158edece37e99bb032cc4b55501d
mrhorin <u0813mb@gmail.com>
2022-11-15 14:43:57 +0900

    Fix including GTM issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppContext, AppProps } from 'next/app'
 import Router from 'next/router'
+import Script from 'next/script'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 import nprogress from 'nprogress'
@@ -26,8 +27,31 @@ Router.events.on('routeChangeError', nprogress.done)
 Router.events.on('routeChangeComplete', nprogress.done)
 
 function MyApp({ Component, pageProps }: AppProps) {
+  function GTMScript(): JSX.Element{
+    if (process.env.NEXT_PUBLIC_ENV == 'production') {
+      return (
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WJZVLVN');
+          `
+          }}
+        />
+      )
+    }
+    return <></>
+  }
+
   return (
     <AlertProvider template={AlertTemplate} {...alertOptions}>
+      {/* Google Tag Manager */}
+      <GTMScript />
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
-import Script from 'next/script'
 
 import nookies from 'nookies'
 
@@ -14,27 +13,6 @@ class MyDocument extends Document<Props> {
     const cookies = nookies.get(ctx)
     const theme = cookies.theme || process.env.NEXT_PUBLIC_DEFAULT_THEME
     return { ...initialProps, theme }
-  }
-
-  GTMScript(): JSX.Element{
-    if (process.env.NEXT_PUBLIC_ENV == 'production') {
-      return (
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WJZVLVN');
-          `
-          }}
-        />
-      )
-    }
-    return <></>
   }
 
   GTMNoscript(): JSX.Element{
@@ -54,10 +32,7 @@ class MyDocument extends Document<Props> {
     const theme = this.props.theme
     return (
       <Html data-theme={theme}>
-        <Head>
-          {/* Google Tag Manager */}
-          <this.GTMScript />
-        </Head>
+        <Head />
         <body>
           {/* Google Tag Manager (noscript) */}
           <this.GTMNoscript />
