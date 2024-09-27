04045c0a5213c213fccc02237e1ced497a41a843
mrhorin <u0813mb@gmail.com>
2024-09-27 20:57:49 +0900

    Fix a script tag for Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       return (
         <Script
           id="google-tag-manager"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
