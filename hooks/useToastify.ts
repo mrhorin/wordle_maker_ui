e4dad3acedebfc234d5546804abe7db306e84fc3
mrhorin <u0813mb@gmail.com>
2024-10-02 21:27:13 +0900

    Fix toastify's theme to match data-theme

--- hooks/useToastify.ts
+++ hooks/useToastify.ts
@@ -1,7 +1,15 @@
 import { toast, Bounce } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import useTheme from 'hooks/useTheme'
 
 export default () => {
+  const { getTheme } = useTheme()
+
+  function theme(): 'light' | 'dark' | 'colored'{
+    if (getTheme() == "dark") return "dark"
+    if (getTheme() == "light") return "light"
+    return "dark"
+  }
 
   function alertSuccess(msg: string): void{
     toast.success(msg, {
@@ -12,7 +20,7 @@ export default () => {
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "dark",
+      theme: theme(),
       transition: Bounce,
     })
   }
@@ -26,7 +34,7 @@ export default () => {
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "dark",
+      theme: theme(),
       transition: Bounce,
     })
   }
